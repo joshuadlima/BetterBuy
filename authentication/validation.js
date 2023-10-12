c9c68a4f64cce3508adbf2a1b63338f2a603bbd5
Atharva Ayir <111686877+atharvaayir@users.noreply.github.com>
2023-10-13 00:23:40 +0530

    Add files via upload

--- authentication/validation.js
+++ authentication/validation.js
@@ -1,6 +1,8 @@
 const passwordInput1 = document.querySelector(".pass-field1 input");
 const requirementList = document.querySelectorAll(".requirement-list li");
-const passwordInput2=document.querySelector(".pass-field2 input");
+const passwordInput2 = document.querySelector(".pass-field2 input");
+const checkBoxElement = document.getElementById('form2Example3c');
+//console.log(checkBoxElement);
 // An array of password requirements with corresponding 
 // regular expressions and index of the requirement list item
 const requirements = [
@@ -10,7 +12,7 @@ const requirements = [
     { regex: /[^A-Za-z0-9]/, index: 3 }, // At least one special character
     { regex: /[A-Z]/, index: 4 }, // At least one uppercase letter
 ]
-let f=[0,0,0,0,0,0];
+let f = [0, 0, 0, 0, 0, 0, 0];
 //console.log(passwordInput2);
 passwordInput1.addEventListener("keyup", (e) => {
     requirements.forEach(item => {
@@ -22,50 +24,57 @@ passwordInput1.addEventListener("keyup", (e) => {
         if (isValid) {
             requirementItem.classList.add("valid");
             requirementItem.firstElementChild.className = "fa-solid fa-check";
-            f[item.index]=1;
+            f[item.index] = 1;
         }
-        else
-         {
-           
+        else {
+
             requirementItem.classList.remove("valid");
             requirementItem.firstElementChild.className = "fa-solid fa-circle";
-            f[item.index]=0;
+            f[item.index] = 0;
         }
     });
 });
-let submitButton=document.getElementById('submitbutton');
+let submitButton = document.getElementById('submitbutton');
+submitButton.disabled = true;
+submitButton.style.cursor = "not-allowed";
 //console.log(submitButton);
-submitButton.disabled = false;
-submitButton.style.cursor="pointer";
-passwordInput2.addEventListener("keyup",checkpassword);
-function checkpassword()
-{
-    if(passwordInput1.value==passwordInput2.value&&passwordInput1.value!="")
-    {
+submitButton.disabled = true;
+submitButton.style.cursor = "not-allowed";
+passwordInput2.addEventListener("keyup", checkpassword);
+checkBoxElement.addEventListener("click",registerButtonCheck);
+function checkpassword() {
+    if (passwordInput1.value == passwordInput2.value && passwordInput1.value != "") {
         requirementList[5].classList.add("valid");
         requirementList[5].firstElementChild.className = "fa-solid fa-check";
-        f[5]=1;
+        f[5] = 1;
+
     }
-    else{
-        
+    else {
+
         requirementList[5].classList.remove("valid");
         requirementList[5].firstElementChild.className = "fa-solid fa-circle";
-        f[5]=0; 
+        f[5] = 0;
     }
-    let p=0;
-    for(let i=0;i<6;i++)
-    {
-        if(f[i]==1)
-        p++;
+
+}
+function registerButtonCheck() {
+    let p = 0;
+    if (checkBoxElement.checked) {
+        f[6] = 1;
     }
-    if(p==6)
-    {
-    submitButton.disabled = false;
-    submitButton.style.cursor="pointer";
+    else {
+        f[6] = 0;
     }
-    else
-    {
+    for (let i = 0; i < 7; i++) {
+        if (f[i] == 1)
+            p++;
+    }
+    if (p == 7) {
+        submitButton.disabled = false;
+        submitButton.style.cursor = "pointer";
+    }
+    else {
         submitButton.disabled = true;
-        submitButton.style.cursor="not-allowed";
+        submitButton.style.cursor = "not-allowed";
     }
 }
