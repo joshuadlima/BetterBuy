7836ac5edf5dd70cab8e3d2d7fd78422d0f06235
joshuadlima <joshd11723@gmail.com>
2023-11-09 01:45:22 +0530

    added favicon and logo

--- authentication/forgot_password_auth.php
+++ authentication/forgot_password_auth.php
@@ -34,6 +34,7 @@ if (isset($_POST['user_auth'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BetterBuy</title>
+    <link rel="icon" type="image/x-icon" href="../resources/favicon.png">
 
     <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
--- homepage/about.php
+++ homepage/about.php
@@ -11,6 +11,7 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BetterBuy</title>
+    <link rel="icon" type="image/x-icon" href="../resources/favicon.png">
 
     <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
