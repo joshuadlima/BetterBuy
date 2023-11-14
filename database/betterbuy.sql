69c914de13707b321e5394e40e579ddea52a2872
joshuadlima <joshd11723@gmail.com>
2023-11-14 17:58:16 +0530

    added file uploads

--- database/betterbuy.sql
+++ database/betterbuy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2023 at 05:38 PM
+-- Generation Time: Nov 14, 2023 at 01:27 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -75,7 +75,7 @@ INSERT INTO `categories` (`category_id`, `category_name`) VALUES
 (2, 'PANTS'),
 (4, 'SHIRTS'),
 (5, 'UNDERWEARS'),
-(6, 'TROUSERS');
+(11, 'DRESSES');
 
 -- --------------------------------------------------------
 
@@ -110,15 +110,21 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_name`, `product_description`, `category_id`, `product_image`, `product_price`) VALUES
-(3, 'Regular Fit Wool-blend jacket', 'Classic elegance meets cozy warmth in our Regular Fit Wool-blend jacket.', 4, 'm1.jpg', 3999),
-(4, 'Regular Fit Jersey top', 'Effortless style and comfort merge in our Regular Fit Jersey Top.', 4, 'm2.jpg', 1799),
-(5, 'Relaxed Fit Jersey Top', 'Unmatched comfort, casual charm: discover our Relaxed Fit Jersey Top', 4, 'm3.jpg', 1499),
-(6, 'Regular Fit Royal Brown Jacket', 'Elevate your style with our Regular Fit Royal Brown Jacket.', 4, 'm4.jpg', 7999),
-(7, 'Oversized pinstriped blazer', 'Make a bold statement with our chic Oversized Pinstriped Blazer', 4, 'w2.jpg', 5999),
-(8, 'Double-Breasted Blazer collection.', 'Sophistication meets style in our timeless Double-Breasted Blazer collection.', 4, 'w3.jpg', 3599),
-(9, 'Crinkled shirt', 'Effortlessly trendy: Explore our collection of Crinkled Shirts for chic style', 4, 'w4.jpg', 1999),
-(10, 'Vintage Mom Fit High Ankle Jeans', 'Embrace retro vibes with these Vintage High Ankle Jeans', 2, 'w1.jpg', 2299),
-(13, '3-pack short cotton trunks', 'Trunks in stretch cotton jersey with flatlock seams', 5, 'm5.jpg', 1299);
+(25, 'Relaxed Fit Tailored trousers', 'Relaxed-fit trousers in twill with a zip fly with a concealed button and hook-and-eye fastening. Diagonal side pockets, jetted back pockets and straight legs with creases.', 2, '../product_images/hmgoepprod.jpg', 3599),
+(26, 'Regular Fit Flannel shirt', 'Regular-fit shirt in soft cotton flannel with a turn-down collar, French front and yoke at the back. Long sleeves with adjustable buttoning at the cuffs, and a rounded hem', 4, '../product_images/hmgoepprod2.jpg', 1999),
+(27, 'Regular Fit Flannel shirt', 'Shirt in soft cotton flannel with a turn-down collar, buttons down the front and a yoke at the back. Regular fit with an open chest pocket and long sleeves with buttoned cuffs and a sleeve placket with a link button. Gently rounded hem.', 4, '../product_images/hmgoepprod3.jpg', 2299),
+(28, 'Slim Fit Corduroy trousers', '5-pocket trousers in soft cotton corduroy. Slim fit with a zip fly and button.', 2, '../product_images/hmgoepprod4.jpg', 2699),
+(29, 'Chunky loafers', 'Loafers with a decorative tab and seam at the front. Canvas linings and insoles and chunky, patterned soles. Heel 3.2 cm.', 1, '../product_images/hmgoepprod5.jpg', 3599),
+(30, 'Oversized corduroy overshirt', 'Oversized overshirt in soft cotton corduroy with a collar and buttons down the front. Dropped shoulders, long sleeves with buttoned cuffs, a patch chest pocket and a rounded hem.', 4, '../product_images/hmgoepprod6.jpg', 2099),
+(31, 'Tie-detail satin dress', 'Short dress in softly draping satin with a V-shaped neckline that has wide ties that tie at the front. Dropped shoulders and long sleeves with wide, buttoned cuffs and a sleeve placket with a link button. Straight-cut hem with a small slit at each side', 11, '../product_images/hmgoepprod7.jpg', 1799),
+(32, 'Frill-trimmed bandeau dress', 'Short, fitted bandeau dress in heavy stretch jersey with two frill trims at the hem, the upper layer in an asymmetric cut.', 11, '../product_images/hmgoepprod8.jpg', 1499),
+(33, 'Flared twill trousers', 'Fitted trousers in stretch cotton twill with a high waist, zip fly and button, fake front pockets and real back pockets. Legs with flared hems.', 11, '../product_images/hmgoepprod9.jpg', 1499),
+(34, 'Skinny High Jeans', '5-pocket jeans in slightly stretchy denim with a high waist, zip fly and button and skinny legs', 2, '../product_images/hmgoepprod10.jpg', 1499),
+(35, 'Linen shirt', 'Loose-fit shirt in airy linen with a collar and buttons down the front. Double-layered yoke, dropped shoulders and long sleeves with buttoned cuffs.', 4, '../product_images/hmgoepprod11.jpg', 2299),
+(36, 'Leather platform sandals', 'Sandals in leather with covered block heels and square toes. Narrow foot straps and a thin, adjustable strap around the ankle. Leather linings and insoles. Platform 2.5 cm. Heel 10 cm.', 1, '../product_images/hmgoepprod12.jpg', 5699),
+(37, 'Cashmere polo-neck dress', 'Calf-length dress in soft, fine-knit cashmere. Relaxed fit with a ribbed polo neck, low dropped shoulders and extra-long sleeves. Straight hem with a slit in each side. Ribbing at the cuffs and hem.', 11, '../product_images/hmgoepprod13.jpg', 14999),
+(38, '3-pack short cotton trunks', 'Trunks in stretch cotton jersey with flatlock seams for added comfort, short legs, covered elastication at the waist and lined front.', 5, '../product_images/m5.jpg', 1299),
+(39, '3-pack cotton short trunks', 'Trunks in stretch cotton jersey with flatlock seams for added comfort. Elasticated waist, a lined front and short legs.', 5, '../product_images/hmgoepprod0.jpg', 1499);
 
 -- --------------------------------------------------------
 
@@ -151,6 +157,15 @@ CREATE TABLE `user_table` (
   `user_address` varchar(255) NOT NULL,
   `user_mobile` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `user_table`
+--
+
+INSERT INTO `user_table` (`user_id`, `username`, `user_email`, `user_password`, `user_ip`, `user_address`, `user_mobile`) VALUES
+(15, 'Joshua', 'joshuadlima123@gmail.com', '2868bae551e9544cf7a7a6406e5428ed', '127.0.0.1', 'Goa College of Engineering - Farmagudi, Ponda, South Goa District, Goa, 403404, India', 2147483647),
+(16, 'Noah', 'joshuadlima123@gmail.com', '12d362c786065eb9d3e3944255d1f242', '127.0.0.1', 'Mapusa Industrial Estate, Duler, Mapusa, Bardez, North Goa District, Goa, 403507, India', 1233121323),
+(17, 'Dlima', 'joshuadlima123@gmail.com', 'b9851a8e9a9ef49deaffa30b38cb7f65', '127.0.0.1', 'Mapusa Industrial Estate, Duler, Mapusa, Bardez, North Goa District, Goa, 403507, India', 2147483647);
 
 --
 -- Indexes for dumped tables
@@ -214,37 +229,37 @@ ALTER TABLE `admin_table`
 -- AUTO_INCREMENT for table `cart_details`
 --
 ALTER TABLE `cart_details`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `ordered_products`
 --
 ALTER TABLE `ordered_products`
-  MODIFY `ordered_products_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `ordered_products_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `user_orders`
 --
 ALTER TABLE `user_orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `user_table`
 --
 ALTER TABLE `user_table`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Constraints for dumped tables
